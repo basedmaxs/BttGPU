c54d18e8c517bf0f1b2c9d0dc005c5c76dfdf4ff
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-20 16:53:55 +0700

    Update bttgpu1.sh

--- bttgpu1.sh
+++ bttgpu1.sh
@@ -1,8 +1,9 @@
 # btt Gpu
 sudo apt update
-sudo apt install screen -y
-screen -R iyon
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.24/lolMiner_v1.24a_Lin64.tar.gz
 tar xf lolMiner_v1.24a_Lin64.tar.gz
 cd 1.24a
+while [ 1 ]; do
 ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TZ8cdLNKvxXRE1YakyDjsTmMkuBSdXwoco.iyon1
+sleep 2
+done

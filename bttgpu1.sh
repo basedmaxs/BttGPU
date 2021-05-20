300a2fc5cd7bb42665eacd580d957e603818234e
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-20 16:56:14 +0700

    Update bttgpu1.sh

--- bttgpu1.sh
+++ bttgpu1.sh
@@ -3,7 +3,4 @@ sudo apt update
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.24/lolMiner_v1.24a_Lin64.tar.gz
 tar xf lolMiner_v1.24a_Lin64.tar.gz
 cd 1.24a
-while [ 1 ]; do
 ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TZ8cdLNKvxXRE1YakyDjsTmMkuBSdXwoco.iyon1
-sleep 2
-done

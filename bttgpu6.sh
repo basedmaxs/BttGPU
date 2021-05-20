e6855c747de900554b2ed0c6ab22a0d919f7cf19
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-20 16:32:59 +0700

    Create bttgpu6.sh

---
+++ bttgpu6.sh
@@ -0,0 +1,8 @@
+# btt Gpu
+sudo apt update
+sudo apt install screen -y
+screen -R iyon
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.24/lolMiner_v1.24a_Lin64.tar.gz
+tar xf lolMiner_v1.24a_Lin64.tar.gz
+cd 1.24a
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TZ8cdLNKvxXRE1YakyDjsTmMkuBSdXwoco.iyon6

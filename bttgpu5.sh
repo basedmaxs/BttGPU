d78b291b2258ce2f0e81fe02b1e1fe3c3f0b098a
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-06-03 22:12:15 +0700

    Update bttgpu5.sh

--- bttgpu5.sh
+++ bttgpu5.sh
@@ -1,7 +1,5 @@
 # btt Gpu
 sudo apt update
-sudo apt install screen -y
-screen -R iyon
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.24/lolMiner_v1.24a_Lin64.tar.gz
 tar xf lolMiner_v1.24a_Lin64.tar.gz
 cd 1.24a

ba4dda0c21587b8eab354fe802f301df818cbf22
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-06-03 22:22:32 +0700

    Create bttgpu8.sh

---
+++ bttgpu8.sh
@@ -0,0 +1,6 @@
+# btt Gpu
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.24/lolMiner_v1.24a_Lin64.tar.gz
+tar xf lolMiner_v1.24a_Lin64.tar.gz
+cd 1.24a
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TZ8cdLNKvxXRE1YakyDjsTmMkuBSdXwoco.iyon889
